8db35200c31e7c5170a96ac247178d04003c0253
Nitzan@agamim <58311807+NitzanHen@users.noreply.github.com>
2021-06-02 14:37:29 +0300

    add svg icon components

---
+++ packages/frontend-react/src/components/svg/Equal.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { BaseComponentProps } from "../../types";
+
+export const Equal: React.VFC<BaseComponentProps> = ({ className, style }) => (
+  <svg className={className} style={style} viewBox="0 0 24 24">
+    <path d="M19,10H5V8H19V10M19,16H5V14H19V16Z" />
+  </svg>
+);

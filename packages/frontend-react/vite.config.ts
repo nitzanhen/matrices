3c544e0de2f0c6b64fee9f9c34dd12bdb0f836fd
NitzanHen <nitzan.alpha@gmail.com>
2021-06-10 20:09:30 +0000

    set up common package

--- packages/frontend-react/vite.config.ts
+++ packages/frontend-react/vite.config.ts
@@ -1,7 +1,20 @@
+//@ts-expect-error path is not known to browser, but script is being run in a Node.Js env.
+import path from 'path';
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [reactRefresh()],
+
+  resolve: {
+    alias: {
+      //@ts-expect-error path is not known to browser, but script is being run in a Node.Js env.
+      '@matrices/common': path.join(__dirname, '..', 'common', 'src', 'index.ts')
+    }
+  },
+
+  build: {
+    outDir: 'build'
+  }
 })

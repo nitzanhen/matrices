f7d6e96938bbd20bda19f84f6a641f4a4c67e6ed
NitzanHen <nitzan.alpha@gmail.com>
2021-06-12 09:18:26 +0000

    Fix typo

--- packages/common/src/Result.ts
+++ packages/common/src/Result.ts
@@ -4,7 +4,7 @@ export class EmptyCellError extends Error {};
 /** @todo maybe find a better field name than result (leads to `result.result`) */
 export type Result<T, E> = { ok: true, result: T } | { ok: false, err: E }
 
-/** Convenience factory for { ok: true, result: result } */
+/** Convenient factory for `{ ok: true, result: result }` */
 export const ok = <T>(result: T): { ok: true, result: T } => ({ ok: true, result });
-/** Convenience factory for { ok: false, err: err } */
+/** Convenient factory for `{ ok: false, err: err }` */
 export const err = <E>(err: E): { ok: false, err: E } => ({ ok: false, err });

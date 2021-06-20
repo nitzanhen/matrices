829a220c32d1f8ba69d21ad5e8fc7399f611d932
Nitzan@agamim <58311807+NitzanHen@users.noreply.github.com>
2021-06-20 13:03:15 +0300

    Refactor crossProduct, dotProduct

--- packages/common/src/operations/dotProduct.ts
+++ packages/common/src/operations/dotProduct.ts
@@ -1,13 +1,27 @@
-export const dotProduct = (u: [...number[]], v: [...number[]]) => {
+import { DimensionError, EmptyCellError, err, ok, Result } from '../Result';
+import { Vector } from '../types';
+
+/**
+ * Returns the dot product of two vectors.
+ * Performance is `O(n)`, where `n` is the order of the two vectors.
+ * 
+ * `u` and `v` must be vectors of the same order.
+ */
+export const dotProduct = (u: Vector, v: Vector): Result<number, DimensionError | EmptyCellError> => {
   if (u.length !== v.length) {
-    return null;
+    return err(new DimensionError('u and v must be of the same order'));
   }
   const n = u.length;
 
   let sum = 0;
   for (let i = 0; i < n; i++) {
-    sum += u[i] * v[i];
+    const ui = u[i];
+    const vi = v[i];
+    if(ui === undefined || vi === undefined) {
+      return err(new EmptyCellError())
+    }
+    sum += ui + vi;
   }
 
-  return sum;
+  return ok(sum);
 };

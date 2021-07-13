231f4a1a589b1ad816b2d0e60780566d0727c41c
Nitzan@agamim <58311807+NitzanHen@users.noreply.github.com>
2021-07-13 15:33:26 +0300

    remove unused svg component

--- packages/frontend-react/src/components/svg/Minus.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { BaseComponentProps } from '../../types';
-
-export const Minus: React.VFC<BaseComponentProps> = ({ className, style }) => (
-  <svg className={className} style={style} viewBox='0 0 24 24'>
-    <path d='M19,13H5V11H19V13Z' />
-  </svg>
-);
